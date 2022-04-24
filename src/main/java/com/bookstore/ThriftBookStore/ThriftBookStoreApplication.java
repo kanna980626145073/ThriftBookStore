185b13e07bc7a856d6c507089abbed8ebedaeee4
KANNA <kannakanna56@yahoo.com.my>
2022-04-24 13:25:54 +0800

    Initial commit with basic structure

---
+++ src/main/java/com/bookstore/ThriftBookStore/ThriftBookStoreApplication.java
@@ -0,0 +1,14 @@
+package com.bookstore.ThriftBookStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class ThriftBookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThriftBookStoreApplication.class, args);
+	}
+
+}
